24e4b5ead324d24b5e3d8c1e3bb6d13487c92648
Iván Avila <ivan.avicamp@gmail.com>
2022-07-16 01:54:00 -0500

    Styling Header

---
+++ src/components/Home/HomeHero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomeHero = () => {
+  return (
+    <div className='home-hero'>
+        <header className='hero__section'>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quam similique id provident, earum, veniam, eligendi consectetur commodi aliquid nesciunt officia. At corporis quod aperiam temporibus debitis. Saepe, voluptates magni?
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default HomeHero
--- src/components/Home/HomeScreen.jsx
+++ src/components/Home/HomeScreen.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import HomeCard from './HomeCard'
+import HomeHero from './HomeHero'
 import './style/homeScreen.css'
 
 const HomeScreen = () => {
 
   return (
     <div className='home'>
-      <div className='home-container'>
-            <HomeCard />
+      <div className='home__hero__container'>
+        <HomeHero />
+      </div>
+      <div className='home__card-container'>
+        <HomeCard />
       </div>
     </div>
   )
