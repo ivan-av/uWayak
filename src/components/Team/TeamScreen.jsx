49552c793491e10c299cf2578f42d5a8b47c0573
Iván Avila <ivan.avicamp@gmail.com>
2022-07-23 19:31:56 -0500

    teamMembers order

--- src/components/Team/TeamScreen.jsx
+++ src/components/Team/TeamScreen.jsx
@@ -8,15 +8,15 @@ const TeamScreen = () => {
   const teamMembers = [
     {
       id: 1,
-      name: 'Julia Carbajal',
-      role: 'Team Lead / Administradora',
-      photo: 'https://source.unsplash.com/random/300x295/?flower'
-    },
-    {
-      id: 2,
       name: 'Arely Coria',
       role: 'Publicista',
       photo: 'https://source.unsplash.com/random/300x300/?flower'
+    },
+    {
+      id: 2,
+      name: 'Eduardo Iniestra',
+      role: 'Administrador',
+      photo: 'https://source.unsplash.com/random/300x295/?fruit'
     },
     {
       id: 3,
@@ -26,9 +26,9 @@ const TeamScreen = () => {
     },
     {
       id: 4,
-      name: 'Eduardo Iniestra',
-      role: 'Administrador',
-      photo: 'https://source.unsplash.com/random/300x295/?fruit'
+      name: 'Iván Avila',
+      role: 'Programador',
+      photo: 'https://source.unsplash.com/random/300x315/?fruit'
     },
     {
       id: 5,
@@ -38,9 +38,9 @@ const TeamScreen = () => {
     },
     {
       id: 6,
-      name: 'Mireya Sánchez',
-      role: 'Psicóloga',
-      photo: 'https://source.unsplash.com/random/300x310/?flower'
+      name: 'Julia Carbajal',
+      role: 'Team Lead / Administradora',
+      photo: 'https://source.unsplash.com/random/300x295/?flower'
     },
     {
       id: 7,
@@ -50,21 +50,21 @@ const TeamScreen = () => {
     },
     {
       id: 8,
+      name: 'Mireya Sánchez',
+      role: 'Psicóloga',
+      photo: 'https://source.unsplash.com/random/300x310/?flower'
+    },
+    {
+      id: 9,
       name: 'Rainier Chavolla',
       role: 'Licenciado en Turismo',
       photo: 'https://source.unsplash.com/random/300x305/?fruit'
     },
     {
-      id: 9,
+      id: 10,
       name: 'Ricardo Cruz',
       role: 'Administrador',
       photo: 'https://source.unsplash.com/random/300x310/?fruit'
-    },
-    {
-      id: 10,
-      name: 'Iván Avila',
-      role: 'Desarrollador',
-      photo: 'https://source.unsplash.com/random/300x315/?fruit'
     }
   ]
 
