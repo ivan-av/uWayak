888704698681152a05b2a38a337ec322444b7edf
Iván Avila <ivan.avicamp@gmail.com>
2022-08-29 19:28:32 -0500

    New input contac

--- src/components/Contact/ContactCard.jsx
+++ src/components/Contact/ContactCard.jsx
@@ -4,7 +4,11 @@ const ContactCard = () => {
   return (
     <article className='contact-card__container'>
       <h2 className='contact-card__message'>Contacto</h2>
-      <p>Correo: ublanket.contacto@gmail.com</p>
+      <form /*onSubmit={searchLocation}*/ className='input__contact'>
+        <input type="text" />
+        <button className='location-input__button'>Search</button>
+      </form>
+      <p>Correo: uwayak.contacto@gmail.com</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa similique, nam mollitia, atque impedit, possimus laboriosam maiores hic eum cupiditate labore! Earum voluptatem ipsum veniam natus beatae repudiandae dolore quae!</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus adipisci placeat autem unde beatae hic, impedit voluptatibus tenetur sit! Nihil assumenda nemo doloribus officia. Quaerat possimus excepturi adipisci. Quam, libero?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum natus unde sed consectetur, rerum similique delectus totam harum tempora molestias porro dolore at dolorum reprehenderit velit quas perspiciatis vero modi?</p>
